f5317acf65032d8f56eb5bbcc5561dcb9d3d00b9
Alex Ross <alex@enplug.com>
2015-07-22 15:14:46 -0700

    updating gh_release

--- grunt/gh_release.js
+++ grunt/gh_release.js
@@ -1,18 +1,17 @@
 module.exports = function (grunt) {
+
     return {
         dist: {
             options: {
                 token: grunt.file.readJSON('grunt/gh.private.json').token,
-                owner: 'aleross',
-                repo: 'DashboardUtils'
+                owner: '<%= pkg.repository.owner %>',
+                repo: '<%= pkg.repository.name %>'
             },
-            release: {
-                tag_name: 'v<%= pkg.version %>',
-                asset: {
-                    name: '<%= pkg.name %>.js-v<%= pkg.version %>.zip',
-                    file: 'archive.zip',
-                    'Content-Type': 'application/zip'
-                }
+            tag_name: 'v<%= pkg.version %>',
+            asset: {
+                name: '<%= pkg.name %>.js-v<%= pkg.version %>.zip',
+                file: 'archive.zip',
+                'Content-Type': 'application/zip'
             }
         }
     }
